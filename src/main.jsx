fad3592263109f6be33ea3741eab2278e15f79a8
gowthamr4-1 <gowtham2151@gmail.com>
2026-02-24 12:42:54 +0530

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react"; // <-- import ChakraProvider
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </HashRouter>
 );
