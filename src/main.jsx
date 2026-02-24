5e98ddb93feae1a8ddca6e9c596c0734e4de60f8
gowthamr4-1 <gowtham2151@gmail.com>
2026-02-24 12:47:33 +0530

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react"; // <-- import ChakraProvider
-
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <HashRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </HashRouter>
+
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+
 );
